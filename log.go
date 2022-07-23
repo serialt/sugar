361fd9c2e6f162f4a2e389499768e5a710764c0a
serialt <t@imau.cc>
2022-07-23 11:06:04 +0800

    feat: 日志库改版，支持直接使用

--- log.go
+++ log.go
@@ -2,12 +2,13 @@ package sugar
 
 import (
 	"os"
-	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
+
+var sugar *zap.SugaredLogger
 
 type Logger struct {
 	LogLevel      string // 日志级别
@@ -17,6 +18,7 @@ type Logger struct {
 	LogMaxBackups int    // 日志文件保留个数
 	LogMaxAge     int    // 单位天
 	LogCompress   bool   // 压缩轮转的日志
+	LogColor      bool   // 日志级别分颜色
 }
 
 func LevelToZapLevel(level string) zapcore.Level {
@@ -30,10 +32,10 @@ func LevelToZapLevel(level string) zapcore.Level {
 		return zapcore.WarnLevel
 	case "error":
 		return zapcore.ErrorLevel
-	case "dpanic":
-		return zapcore.DPanicLevel
-	case "panic":
-		return zapcore.PanicLevel
+	// case "dpanic":
+	// 	return zapcore.DPanicLevel
+	// case "panic":
+	// 	return zapcore.PanicLevel
 	case "fatal":
 		return zapcore.FatalLevel
 	default:
@@ -58,13 +60,30 @@ func (lg *Logger) NewMyLogger() *zap.Logger {
 		MessageKey:    "msg",
 		StacktraceKey: "stacktrace",
 		LineEnding:    zapcore.DefaultLineEnding,
-		EncodeLevel:   zapcore.LowercaseLevelEncoder, // zapcore.CapitalLevelEncoder //按级别显示不同颜色
-		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-			enc.AppendString(t.Format("2006-01-02 15:04:05")) //指定时间格式
-		},
-		EncodeDuration: zapcore.SecondsDurationEncoder,
-		EncodeCaller:   zapcore.ShortCallerEncoder,
+		//  zapcore.LowercaseLevelEncoder 小写日志级别无颜色
+		//  zapcore.LowercaseColorLevelEncoder 小写日志级别无颜色
+		//  zapcore.CapitalLevelEncoder
+		//  zapcore.CapitalColorLevelEncoder
+		EncodeLevel: zapcore.CapitalLevelEncoder,
+		// EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		// 	enc.AppendString(t.Format("2006-01-02 15:04:05 Mon")) //指定时间格式
+		// },
+		EncodeTime:     zapcore.RFC3339TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder, // 执行消耗的时间转化成浮点型的秒
+		EncodeCaller:   zapcore.ShortCallerEncoder,     // 以包/文件:行号 格式化调用堆栈
 	}
+	// 简易设置日志级别分颜色
+	if lg.LogColor {
+		// capital, capitalColor, color, default LowercaseLevelEncoder
+		encoderConfig.EncodeLevel.UnmarshalText([]byte("capitalColor"))
+	}
+	// 简易设置日志时间
+	// // RFC3339Nano 2022-07-23T10:49:00.656748+08:00
+	// // RFC3339     2022-07-23T10:49:47+08:00
+	// // ISO8601     2022-07-23T10:50:14.760+0800
+	// // millis      1.6585446469065e+12
+	// // nanos       1658544668921929000
+	// encoderConfig.EncodeTime.UnmarshalText([]byte("RFC3339"))
 
 	// 日志输出类型
 	var encoder zapcore.Encoder
@@ -96,22 +115,87 @@ func (lg *Logger) NewMyLogger() *zap.Logger {
 }
 
 // NewLogger 自定日志配置可以参考此方法
-func NewLogger(logLevel, logFile string) *zap.Logger {
+func NewLogger(logLevel, logFile, logtype string, logColor bool) *zap.Logger {
 	lg := &Logger{
 		LogLevel:      logLevel,
 		LogFile:       logFile,
-		LogType:       "txt",
+		LogType:       logtype,
 		LogMaxSize:    50,
 		LogMaxBackups: 10,
 		LogMaxAge:     365,
 		LogCompress:   true,
+		LogColor:      logColor,
 	}
 	return lg.NewMyLogger()
 }
 
+func init() {
+	sugar = NewLogger("info", "", "", false).Sugar()
+}
+
 // NewSugarLogger 创建一个sugar
-func NewSugarLogger(logLevel, logFile string) *zap.SugaredLogger {
-	sugarLog := NewLogger(logLevel, logFile)
+func NewSugarLogger(logLevel, logFile, logtype string, logColor bool) *zap.SugaredLogger {
+	sugarLog := NewLogger(logLevel, logFile, logtype, logColor)
 	return sugarLog.Sugar()
 
+}
+
+func Debug(args ...interface{}) {
+	sugar.Debug(args...)
+}
+
+func Debugf(temp string, args ...interface{}) {
+	sugar.Debugf(temp, args...)
+}
+
+func Debugw(msg string, keysAndValues ...interface{}) {
+	sugar.Debugw(msg, keysAndValues...)
+}
+
+func Info(args ...interface{}) {
+	sugar.Info(args...)
+}
+
+func Infof(temp string, args ...interface{}) {
+	sugar.Infof(temp, args...)
+}
+
+func Infow(msg string, keysAndValues ...interface{}) {
+	sugar.Infow(msg, keysAndValues...)
+}
+
+func Warn(args ...interface{}) {
+	sugar.Warn(args...)
+}
+
+func Warnf(temp string, args ...interface{}) {
+	sugar.Warnf(temp, args...)
+}
+
+func Warnw(msg string, keysAndValues ...interface{}) {
+	sugar.Warnw(msg, keysAndValues...)
+}
+
+func Error(args ...interface{}) {
+	sugar.Error(args...)
+}
+
+func Errorf(temp string, args ...interface{}) {
+	sugar.Errorf(temp, args...)
+}
+
+func Errorw(msg string, keysAndValues ...interface{}) {
+	sugar.Errorw(msg, keysAndValues...)
+}
+
+func Fatal(args ...interface{}) {
+	sugar.Fatal(args...)
+}
+
+func Fatalf(temp string, args ...interface{}) {
+	sugar.Fatalf(temp, args...)
+}
+
+func Fatalw(msg string, keysAndValues ...interface{}) {
+	sugar.Fatalw(msg, keysAndValues...)
 }
